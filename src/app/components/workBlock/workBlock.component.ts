c4a4383f6d6e2c708b25b005e207394df016dc23
Mohamed Ben Al-fakih <Mohamed.BenAlfakih55@thehutgroup.com>
2022-05-15 20:58:56 +0100

    modified workblock css

--- src/app/components/workBlock/workBlock.component.ts
+++ src/app/components/workBlock/workBlock.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'workBlock',
@@ -6,9 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./workBlock.component.scss']
 })
 export class WorkBlockComponent {
-
+  @Input()
   public jobTitle: string ='';
+
+  @Input()
   public company: string = '';
+
+  @Input()
   public content: string = '';
 
 }
