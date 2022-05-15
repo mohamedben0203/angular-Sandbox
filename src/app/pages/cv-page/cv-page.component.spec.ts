2fac133074b7d10dba44dff65674cca4d471867b
mohamed ben al-fakih <mohamedben0203@gmail.com>
2022-05-15 15:18:20 +0100

    initial components and pages made

---
+++ src/app/pages/cv-page/cv-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CvPageComponent } from './cv-page.component';
+
+describe('CvPageComponent', () => {
+  let component: CvPageComponent;
+  let fixture: ComponentFixture<CvPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CvPageComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CvPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
